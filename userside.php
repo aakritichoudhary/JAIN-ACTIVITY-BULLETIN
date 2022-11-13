5e4d5e11cd03b17b697a6db4413149742081b873
Shruti2468 <88126240+Shruti2468@users.noreply.github.com>
2022-11-13 16:39:36 +0530

    Add files via upload

---
+++ userside.php
@@ -0,0 +1,16 @@
+
+<?php
+include 'config.php';
+
+			$sql = "SELECT * FROM demo";
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) {
+			  // output data of each row
+			  while($row = $result->fetch_assoc()) {
+			   
+		?>
+		<h3><?php echo $row['name']; ?></h3>
+		<p><?php echo $row['message'] ?></p>
+
+		<?php } } ?>
